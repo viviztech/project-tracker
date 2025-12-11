fd42c1e400f1c317fbe71918fdd44469a24fa3af
viviztech <viviztechnologies@gmail.com>
2025-12-11 22:37:06 +0530

    Configure dynamic CORS for production

--- server/index.js
+++ server/index.js
@@ -16,8 +16,12 @@ app.use((req, res, next) => {
 });
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'], // Vite default port
-    credentials: true
+    origin: process.env.NODE_ENV === 'production'
+        ? [process.env.CLIENT_URL]
+        : ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 app.use(cookieParser());
 
